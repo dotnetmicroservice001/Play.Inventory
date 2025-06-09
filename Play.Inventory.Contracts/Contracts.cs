633aeeb83589ef9562177ac4e9948e97abe61fa5
basnets24 <sneha.basnet2@gmail.com>
2025-06-09 15:46:07 -0400

    message contract file init

---
+++ Play.Inventory.Contracts/Contracts.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Play.Inventory.Contracts;
+
+
+
+public record GrantItems(Guid UserId, 
+                Guid CatalogItemId,
+                int Quantity,
+                Guid CorrelationId);
+
+public record InventoryItemsGranted(Guid CorrelationId);       
+
+//compensatory action for granting 
+public record SubtractItems(Guid UserId, 
+    Guid CatalogItemId,
+    int Quantity,
+    Guid CorrelationId);        
+
+public record InventoryItemsSubtracted(Guid CorrelationId);    
